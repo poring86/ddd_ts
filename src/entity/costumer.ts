f0e4f741aea1c6757c0b98d376c68231301ed96f
Matheus Lino <matheuslino86a@gmail.com>
2023-03-12 17:10:39 -0400

    feat: :sparkles: First commit

---
+++ src/entity/costumer.ts
@@ -0,0 +1,43 @@
+import Address from "./address";
+
+export default class Customer {
+  _id: string;
+  _name: string;
+  _address: Address;
+  _active: boolean;
+
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  activate() {
+    if (this._address === undefined) {
+      throw new Error("Address is mandatory to activate a costumer");
+    }
+    this._active = true;
+  }
+  deactivate() {
+    this._active = false;
+  }
+
+  set Address(address: Address) {
+    this._address = address;
+  }
+}
+
+let customer = new Customer("123", "");
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import Customer from "./entity/costumer";
+import Address from "./entity/address";
+import OrderItem from "./entity/order_item";
+import Order from "./entity/order";
+let customer = new Customer("123", "Matheus");
+const address = new Address("Rua 3", 3, "12312-409", "Mato Grosso do Sul");
+customer.Address = address;
+customer.activate();
+
+const item1 = new OrderItem("1", "Item 1", 10);
+const item2 = new OrderItem("2", "Item 2", 15);
+const order = new Order("1", "123", [item1, item2]);
