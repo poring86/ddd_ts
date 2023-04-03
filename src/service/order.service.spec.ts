0aba11b2ad605cb0fa2538232df959ace2d0ed7b
Matheus Lino <matheuslino86@gmail.com.br>
2023-04-03 17:49:44 -0400

    feat: :sparkles: Reward points

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,9 +1,19 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
 
 describe("Order service unit tests", () => {
-  it("sould get total of all orders", () => {
+  it("should place an order", () => {
+    const custumer = new Customer("c1", "Customer 1");
+    const item1 = new OrderItem("i1", "Item 1", 10, "p1", 1);
+
+    const order = OrderService.placeOrder(custumer, [item1]);
+
+    expect(custumer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  });
+  it("should get total of all orders", () => {
     const item1 = new OrderItem("i1", "Item 1", 100, "p1", 1);
     const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
 
--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,5 +1,17 @@
+import OrderItem from "src/entity/order_item";
+import Customer from "../entity/customer";
 import Order from "../entity/order";
+import { v4 as uuid } from "uuid";
 export default class OrderService {
+  static placeOrder(customer: Customer, items: OrderItem[]): Order {
+    if (items.length === 0) {
+      throw new Error("Items are required");
+    }
+    const order = new Order(uuid(), customer.id, items);
+    customer.addRewardPoints(order.total() / 2);
+    return order;
+  }
+
   static total(orders: Order[]): number {
     return orders.reduce((acc, order) => acc + order.total(), 0);
   }
