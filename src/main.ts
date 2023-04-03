2683476d5b9f68502548c86c8645fd193224432a
Matheus Lino <matheuslino86@gmail.com.br>
2023-04-03 14:04:53 -0400

    improve config

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ const address = new Address("Rua 3", 3, "12312-409", "Mato Grosso do Sul");
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 100);
+const item2 = new OrderItem("2", "Item 2", 15, "p1", 200);
 const order = new Order("1", "123", [item1, item2]);
